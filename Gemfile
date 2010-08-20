ea1c0325b865ce43d85433c1562441bc4d96c648
dinedine <didier.lafforgue@gmail.com>
2010-08-20 15:57:39 +0200

    first commit (for old revisions, see locomotive engine repo)

---
+++ Gemfile
@@ -0,0 +1,12 @@
+source 'http://gemcutter.org'
+
+gem 'bson_ext', '>= 1.0.1'
+gem 'mongo_ext'
+gem 'mongoid', '2.0.0.beta.16'
+gem 'activesupport', '~>3.0.0.rc'
+gem 'carrierwave-rails3', :require => 'carrierwave'
+
+group :test do
+  gem 'rspec', '>= 2.0.0.beta.10'
+  gem 'mocha', :git => 'git://github.com/floehopper/mocha.git'
+end
---
+++ spec/integration/types/category_spec.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+
+describe CustomFields::Types::Category do
+  
+  before(:each) do
+    @project = Project.new(:name => 'Locomotive')
+    @field = @project.task_custom_fields.build(:label => 'Main category', :_alias => 'main_category', :kind => 'Category')
+  end
+    
+  context 'saving category items' do
+
+    before(:each) do
+      @field.category_items.build :name => 'Development'
+      @field.category_items.build :name => 'Design'
+      @field.updated_at = Time.now
+    end
+  
+    it 'persists items' do
+      @field.save.should be_true
+      @project.reload
+      @project.task_custom_fields.first.category_items.size.should == 2
+    end
+    
+    context 'assigning a category and persists' do
+    
+      it 'sets the category from a category name' do
+        task = @project.tasks.build(:main_category => 'Design')
+        task.save && @project.reload
+        @project.tasks.first.main_category.should == 'Design'
+      end
+    
+      it 'sets the category from an id (string)' do
+        task = @project.tasks.build(:main_category => @field.category_items.last._id.to_s)
+        task.save && @project.reload
+        @project.tasks.first.main_category.should == 'Design'
+      end
+    
+      it 'sets the category from an id (BSON::ObjectID)' do
+        task = @project.tasks.build(:main_category => @field.category_items.last._id)
+        task.save && @project.reload
+        @project.tasks.first.main_category.should == 'Design'
+      end
+    
+    end
+    
+  end
+  
+end
