88ad0825a99feaf9f37c85101d3f82b3ac5f4907
dinedine <didier.lafforgue@gmail.com>
2010-10-04 15:44:33 +0200

    downcase kind field value

--- lib/custom_fields/custom_fields_for.rb
+++ lib/custom_fields/custom_fields_for.rb
@@ -1,11 +1,11 @@
 module CustomFields
-  
+
   module CustomFieldsFor
-    
+
     def self.included(base)
       base.extend(ClassMethods)
     end
-    
+
     # Enhance an embedded collection by providing methods to manage custom fields
     #
     # class Company
@@ -18,33 +18,33 @@ module CustomFields
     #    field :name, String
     # end
     #
-    # company.employee_custom_fields.build :label => 'His/her position', :_alias => 'position', :kind => 'String'
+    # company.employee_custom_fields.build :label => 'His/her position', :_alias => 'position', :kind => 'string'
     #
     # company.employees.build :name => 'Michael Scott', :position => 'Regional manager'
     #
     module ClassMethods
-      
+
       def custom_fields_for(collection_name)
         singular_name = collection_name.to_s.singularize
-        
+
         class_eval <<-EOV
           field :#{singular_name}_custom_fields_counter, :type => Integer, :default => 0
-          
+
           embeds_many :#{singular_name}_custom_fields, :class_name => "::CustomFields::Field"
-          
+
           validates_associated :#{singular_name}_custom_fields
-          
+
           accepts_nested_attributes_for :#{singular_name}_custom_fields, :allow_destroy => true
-          
+
           def ordered_#{singular_name}_custom_fields
             self.#{singular_name}_custom_fields.sort { |a, b| (a.position || 0) <=> (b.position || 0) }
           end
-          
+
         EOV
       end
-      
+
     end
-    
+
   end
-  
+
 end
