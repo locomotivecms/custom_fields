b41ec5da01a9c4a785583c5b24f3cc1a39699257
dinedine <didier.lafforgue@gmail.com>
2011-02-01 01:31:09 +0100

    the Person model used in tests was not set up correctly

--- spec/models/person.rb
+++ spec/models/person.rb
@@ -1,10 +1,10 @@
 class Person
-  
+
   include Mongoid::Document
   include CustomFields::ProxyClassEnabler
-  
+
   field :full_name
-  
-  embedded_in :project, :inverse_of => :people
-  
+
+  referenced_in :project, :inverse_of => :people
+
 end
