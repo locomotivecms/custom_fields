afed91108c1c6c11e21c62b945293e7631fca540
did <didier.lafforgue@gmail.com>
2012-08-12 18:30:12 +0200

    fix issue #10

--- lib/custom_fields/types/boolean.rb
+++ lib/custom_fields/types/boolean.rb
@@ -12,17 +12,13 @@ module CustomFields
 
         module ClassMethods
 
-          # Adds a boolean field
+          # Adds a boolean field. It can not be required.
           #
           # @param [ Class ] klass The class to modify
-          # @param [ Hash ] rule It contains the name of the field and if it is required or not
+          # @param [ Hash ] rule It contains the name of the field.
           #
           def apply_boolean_custom_field(klass, rule)
             klass.field rule['name'], :type => ::Boolean, :localize => rule['localized'] || false, :default => false
-
-            if rule['required']
-              klass.validates_presence_of rule['name']
-            end
           end
 
         end
