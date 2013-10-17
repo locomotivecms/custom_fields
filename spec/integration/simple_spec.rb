76ddb41a44d7a7139b3756fdfd981b5aa8854566
did <didier.lafforgue@gmail.com>
2013-10-18 00:50:25 +0200

    rspec tests to illustrate the issue #23

---
+++ spec/integration/simple_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe 'CustomFieldsFor' do
+
+  let(:blog) { create_blog }
+
+  let(:a_post) { Post.create(title: 'LOL cat', body: 'Lorem ipsum') }
+
+  describe 'post' do
+
+    subject { blog.posts.create(title: 'Hello world', body: 'Lorem ipsum') }
+
+    its(:persisted?) { should be_true }
+    its(:title) { should == 'Hello world' }
+
+  end
+
+  describe 'a post without a blog' do
+
+    before { a_post }
+
+    subject { Post.find(a_post._id) }
+
+    its(:title) { should == 'LOL cat' }
+
+  end
+
+  def create_blog
+    Blog.create(name: 'My personal blog')
+  end
+end
