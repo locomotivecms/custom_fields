bebec66b14a842bcec1af7c59169f5990daa4ad1
Joel AZEMAR <joel.azemar@gmail.com>
2014-08-07 12:35:49 +0200

    update monkey patch

--- lib/custom_fields/extensions/mongoid/factory.rb
+++ lib/custom_fields/extensions/mongoid/factory.rb
@@ -4,17 +4,16 @@ module Mongoid #:nodoc:
   # Instantiates documents that came from the database.
   module Factory
 
-    def from_db_with_custom_fields(klass, attributes = {}, criteria_instance_id = nil)
+    def from_db_with_custom_fields(klass, attributes = nil, selected_fields = nil)
       if klass.with_custom_fields?
         klass.klass_with_custom_fields(attributes['custom_fields_recipe'])
       end
-      from_db_without_custom_fields(klass, attributes, criteria_instance_id)
+
+      from_db_without_custom_fields(klass, attributes, selected_fields)
     end
 
     # equivalent for "alias_method_chain :from_db, :custom_fields"
     alias_method :from_db_without_custom_fields, :from_db unless method_defined?(:from_db_without_custom_fields)
     alias_method :from_db, :from_db_with_custom_fields
-
   end
-
 end
